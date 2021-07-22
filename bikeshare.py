eaa6159cfe0ef6bbe1d1481f3ca14bdfa736f97f
Ely Schudrich <schudriche1@montclair.edu>
2021-07-22 11:23:37 -0400

    Add comment at beginning of time stats

--- bikeshare.py
+++ bikeshare.py
@@ -93,7 +93,7 @@ def load_data(city, month, day):
 
     return df
 
-
+#This is where time statistics are computed
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
